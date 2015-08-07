302ef7dc10e1c734146a819a6f56092a80a55acb
Edward Muller <edwardam@interlix.com>
2015-08-07 13:25:26 -0700

    Switch to using a CookieStore instead of a BoltDB store
    This is (or something similar) is required if there are multiple splunk
    indexers as the state can't be saved to a single node.
    
    To deploy you'll need to set a SESSION_ENCRYPTION_KEY first. It may also
    be wise to change the COOKIE_NAME as I'm not sure if we'll be able to
    use the old cookie that boltdb used.
    
    This is untested, but should work.

--- main.go
+++ main.go
@@ -7,65 +7,55 @@ import (
 	"net/url"
 	"strings"
 
-	"github.com/boltdb/bolt"
 	"github.com/go-martini/martini"
-	gsessions "github.com/gorilla/sessions"
 	"github.com/joeshaw/envdecode"
 	"github.com/martini-contrib/sessions"
 	"github.com/tomsteele/dmv"
-	"github.com/yosssi/boltstore/reaper"
-	bstore "github.com/yosssi/boltstore/store"
 )
 
 type config struct {
-	clientID      string `env:"CLIENT_ID,required"`      // Google Client ID
-	clientSecret  string `env:"CLIENT_SECRET,required"`  // Google Client Secret
-	sessionSecret string `env:"SESSION_SECRET,required"` // Random session encruption token
-	dnsName       string `env:"DNS_NAME,required"`       // Public facing DNS Hostname
-
-	sessionDBPath string `env:"SESSION_DB_PATH,default=./sessions.db"` // Path to session database, including db name
-	cookieMaxAge  int    `env:"COOKIE_MAX_AGE,default=1440"`           // Cookie MaxAge, Defaults to 1 day
-	cookieName    string `env:"COOKIE_NAME,default=sproxy_session"`    // The name of the cookie
-
-	proxyURL string `env:"PROXY_URL,default=http://localhost:8000/"` // URL to Proxy to
-
-	callBackPath string `env:"CALLBACK_PATH,default=/auth/callback/google"` // Callback URL
-	authPath     string `env:"AUTH_PATH,default=/auth/google"`              // Auth Path
-
-	healthCheckPath string `env:"HEALTH_CHECK_PATH,default=/en-US/static/html/credit.html"` // Health Check path in splunk, this path is proxied w/o auth. The default is a static file served by the splunk web server
-
-	emailSuffix string `env:"EMAIL_SUFFIX,default=@heroku.com"` // Required email suffix. Emails w/o this suffix will not be let in
+	clientID              string `env:"CLIENT_ID,required"`                                       // Google Client ID
+	clientSecret          string `env:"CLIENT_SECRET,required"`                                   // Google Client Secret
+	sessionSecret         string `env:"SESSION_SECRET,required"`                                  // Random session auth key
+	sessionEncrypttionKey string `env:"SESSION_ENCRYPTION_KEY,required"`                          // Random session encryption key
+	dnsName               string `env:"DNS_NAME,required"`                                        // Public facing DNS Hostname
+	cookieMaxAge          int    `env:"COOKIE_MAX_AGE,default=1440"`                              // Cookie MaxAge, Defaults to 1 day
+	cookieName            string `env:"COOKIE_NAME,default=sproxy_session"`                       // The name of the cookie
+	proxyURL              string `env:"PROXY_URL,default=http://localhost:8000/"`                 // URL to Proxy to
+	callBackPath          string `env:"CALLBACK_PATH,default=/auth/callback/google"`              // Callback URL
+	authPath              string `env:"AUTH_PATH,default=/auth/google"`                           // Auth Path
+	healthCheckPath       string `env:"HEALTH_CHECK_PATH,default=/en-US/static/html/credit.html"` // Health Check path in splunk, this path is proxied w/o auth. The default is a static file served by the splunk web server
+	emailSuffix           string `env:"EMAIL_SUFFIX,default=@heroku.com"`                         // Required email suffix. Emails w/o this suffix will not be let in
 }
 
 var (
-	cfg              = config{}
-	oAuthScopes      = []string{"https://www.googleapis.com/auth/userinfo.email", "https://www.googleapis.com/auth/userinfo.profile"}
-	sessionOptions   = gsessions.Options{Secure: true, HttpOnly: true}
-	boltStoreOptions = bstore.Config{SessionOptions: sessionOptions}
+	cfg            = config{}
+	oAuthScopes    = []string{"https://www.googleapis.com/auth/userinfo.email", "https://www.googleapis.com/auth/userinfo.profile"}
+	sessionOptions = sessions.Options{Secure: true, HttpOnly: true}
 )
 
 // Authorize the user based on email and a set OpenIDUser
-func authorize(s sessions.Session, rw http.ResponseWriter, req *http.Request) {
+func authorize(s sessions.Session, w http.ResponseWriter, r *http.Request) {
 	email := s.Get("email")
 	if email == nil || !strings.HasSuffix(email.(string), cfg.emailSuffix) {
-		http.Redirect(rw, req, cfg.authPath, http.StatusFound)
+		http.Redirect(w, r, cfg.authPath, http.StatusFound)
 		return
 	}
 
 	openIDUser := s.Get("OpenIDUser")
 	if openIDUser != nil && openIDUser != "" {
-		req.Header.Set("X-Openid-User", openIDUser.(string))
+		r.Header.Set("X-Openid-User", openIDUser.(string))
 	} else {
 		// No openIDUser set, so abort and restart the auth flow
-		http.Redirect(rw, req, cfg.authPath, http.StatusFound)
+		http.Redirect(w, r, cfg.authPath, http.StatusFound)
 	}
 }
 
 // Set the OpenIDUser and other session values based on the data from Google
-func handleCallback(goog *dmv.Google, s sessions.Session, rw http.ResponseWriter, req *http.Request) {
+func handleCallback(goog *dmv.Google, s sessions.Session, w http.ResponseWriter, r *http.Request) {
 	// Handle any errors.
 	if len(goog.Errors) > 0 {
-		http.Error(rw, "Oauth failure", http.StatusInternalServerError)
+		http.Error(w, "Oauth failure", http.StatusInternalServerError)
 		return
 	}
 
@@ -74,35 +64,41 @@ func handleCallback(goog *dmv.Google, s sessions.Session, rw http.ResponseWriter
 
 	parts := strings.SplitN(goog.Profile.Email, "@", 2)
 	if len(parts) < 2 {
-		http.Error(rw, "Unable to determine OpenIDUser from email `"+goog.Profile.Email+"`", http.StatusInternalServerError)
+		http.Error(w, "Unable to determine OpenIDUser from email `"+goog.Profile.Email+"`", http.StatusInternalServerError)
 		return
 	}
 	s.Set("OpenIDUser", strings.ToLower(parts[0]))
 
-	http.Redirect(rw, req, "/", http.StatusFound)
+	http.Redirect(w, r, "/", http.StatusFound)
 }
 
 // Ensure that we're being proxied to ourselves via https
-func enforceXForwardedProto(rw http.ResponseWriter, req *http.Request) {
-	xff := req.Header.Get("X-Forwarded-Proto")
+func enforceXForwardedProto(w http.ResponseWriter, r *http.Request) {
+	xff := r.Header.Get("X-Forwarded-Proto")
 	if xff != "https" {
 		u := new(url.URL)
-		*u = *req.URL
+		*u = *r.URL
 		u.Scheme = "https"
 		if u.Host == "" {
-			u.Host = req.Host
+			u.Host = r.Host
 		}
 
-		http.Redirect(rw, req, u.String(), http.StatusFound)
+		http.Redirect(w, r, u.String(), http.StatusFound)
 	}
 }
 
 func main() {
-
 	err := envdecode.Decode(&cfg)
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	switch len([]byte(cfg.sessionEncrypttionKey)) {
+	case 16, 24, 32:
+	default:
+		log.Fatal("Length of SESSION_ENCRYPTION_KEY is not 16, 24 or 32")
+	}
+
 	sessionOptions.MaxAge = cfg.cookieMaxAge
 
 	googleOpts := &dmv.OAuth2Options{
@@ -112,14 +108,6 @@ func main() {
 		Scopes:       oAuthScopes,
 	}
 
-	sessionDB, err := bolt.Open(cfg.sessionDBPath, 0666, nil)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	defer sessionDB.Close()
-	defer reaper.Quit(reaper.Run(sessionDB, reaper.Options{}))
-
 	m := martini.Classic()
 
 	pURL, err := url.Parse(cfg.proxyURL)
@@ -128,10 +116,10 @@ func main() {
 		log.Fatal(err)
 	}
 
-	store, err := bstore.New(sessionDB, boltStoreOptions, []byte(cfg.sessionSecret))
-	if err != nil {
-		log.Fatal(err)
-	}
+	store := sessions.NewCookieStore(
+		[]byte(cfg.sessionSecret),
+		[]byte(cfg.sessionEncrypttionKey),
+	)
 
 	// Inject a session when it's needed
 	m.Use(sessions.Sessions(cfg.cookieName, store))
