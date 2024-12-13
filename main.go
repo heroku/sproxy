630b093546c66159450cf32afaca910df14cb98c
Karunasri Garigipati <35507587+kgarigipati@users.noreply.github.com>
2024-12-12 17:23:41 -0800

    Update main.go
    update var name

--- main.go
+++ main.go
@@ -190,8 +190,8 @@ func handleGoogleCallback(s sessions.Store) http.Handler {
 
 		session.Values["email"] = gp.Email
 		session.Values["GoogleID"] = gp.ID
-		SessionValidTimeInMin := time.Duration(config.SessionValidTime)
-		session.Values["valid_until"] = time.Now().UTC().Add(SessionValidTimeInMin * time.Minute)
+		SessionValidTimeInNS := time.Duration(config.SessionValidTime)
+		session.Values["valid_until"] = time.Now().UTC().Add(SessionValidTimeInNS * time.Minute)
 
 		parts := strings.SplitN(gp.Email, "@", 2)
 		if len(parts) < 2 {
